99e83370b4b7b2b37cebaf7bf0556d2d14fdf77d
Mr. Hands <mrhands31@gmail.com>
2020-10-27 08:56:37 +0100

    Apply active block from cards to players.

--- src/App.js
+++ src/App.js
@@ -32,6 +32,42 @@ class App extends React.Component {
 		this.handleStartNewGame = this.startNewGame.bind(this);
 	}
 
+	getActiveEffectBlock(card) {
+		let { streak } = this.state;
+
+		let defaultBlock = card.effects.find((effect) => {
+			return !effect.condition;
+		});
+
+		let block = defaultBlock;
+
+		let conditionalBlock = card.effects.find((effect) => {
+			return effect.condition;
+		});
+		if (conditionalBlock) {
+			let { check, value } = conditionalBlock.condition;
+
+			let valid = false;
+			if (check === 'equal') {
+				valid = streak === value;
+			} else if (check === 'greater-than') {
+				valid = streak > value;
+			} else if (check === 'greater-than-equal') {
+				valid = streak >= value;
+			}  else if (check === 'less-than') {
+				valid = streak < value;
+			} else if (check === 'less-than-equal') {
+				valid = streak <= value;
+			}
+
+			if (valid) {
+				block = conditionalBlock;
+			}
+		}
+
+		return block;
+	}
+
 	startNewGame() {
 		let { captain, crew } = this.state;
 
--- src/Player.js
+++ src/Player.js
@@ -6,11 +6,51 @@ class Player {
 		this.arousal = 0;
 		this.deck = [];
 		this.hand = [];
-    }
-    
-    applyCard(card, turn, streak) {
-        this.arousal += streak;
-    }
+	}
+
+	getActiveEffectBlock(card, streak) {
+		let defaultBlock = card.effects.find((effect) => {
+			return !effect.condition;
+		});
+
+		let block = defaultBlock;
+
+		let conditionalBlock = card.effects.find((effect) => {
+			return effect.condition;
+		});
+		if (conditionalBlock) {
+			let { check, value } = conditionalBlock.condition;
+
+			let valid = false;
+			if (check === 'equal') {
+				valid = streak === value;
+			} else if (check === 'greater-than') {
+				valid = streak > value;
+			} else if (check === 'greater-than-equal') {
+				valid = streak >= value;
+			}  else if (check === 'less-than') {
+				valid = streak < value;
+			} else if (check === 'less-than-equal') {
+				valid = streak <= value;
+			}
+
+			if (valid) {
+				block = conditionalBlock;
+			}
+		}
+
+		return block;
+	}
+	
+	applyCard(card, turn, streak) {
+		let block = this.getActiveEffectBlock(card, streak);
+
+		for (const [key, value] of Object.entries(block.stats)) {
+			if (key === 'mine' && turn === this.id) {
+				this.arousal += streak * value;
+			}
+		}
+	}
 };
 
 export default Player;
