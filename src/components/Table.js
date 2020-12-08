a4ba9211e1de05cb5e11ff4fb5e143d0df70350b
Mr. Hands <mrhands31@gmail.com>
2020-12-08 10:41:55 +0100

    Display previous card only when available.

--- src/components/Table.js
+++ src/components/Table.js
@@ -6,11 +6,22 @@ import './Table.css';
 
 class Table extends React.Component {
 	render() {
-		const { cardPrevious, tableCards, turn, streak, onResolve } = this.props;
+		const {
+			cardPrevious,
+			tableCards,
+			turn, 
+			streak,
+			onResolve
+		} = this.props;
+
+		let previous;
+		if (cardPrevious) {
+			previous = <Card id={cardPrevious} turn={turn} streak={streak} onResolve={onResolve} />;
+		}
 
 		return (
 			<ul className="m-table">
-				<Card id={cardPrevious} turn={turn} streak={streak} onResolve={onResolve} />
+				{previous}
 				{tableCards.map((card, index) => {
 					return <Card id={card} key={`table-${index}`} turn={turn} streak={streak} onResolve={onResolve} />
 				})}
